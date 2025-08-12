156affe09b791948ed2b9d9b0b89f91052bab27f
revaldirahmatmulya <revaldiaye@gmail.com>
2025-08-12 11:22:05 +0700

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/suitshop-website",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
